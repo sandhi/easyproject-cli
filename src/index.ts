cdc2bb818b2e3d0902e84bbf33943d99cd92d68f
Sandhi <sandhi.yz@gmail.com>
2020-12-28 16:38:17 +0700

    install chalk

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var inquirer = require("inquirer");
+var chalk = require("chalk");
 
 inquirer
     .prompt([
@@ -11,4 +12,4 @@ inquirer
             choices: ["ionic 4", "ionic 5", "loopback 4"],
         },
     ])
-    .then((res: any) => console.log("project yang akan anda buat adalah " + res.project));
+    .then((res: any) => console.log("project yang akan anda buat adalah " + chalk.blue(res.project)));
