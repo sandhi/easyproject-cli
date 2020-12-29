940ec3864815c771bac6f6b4dea6e654b62fbbb3
Sandhi <sandhi.yz@gmail.com>
2020-12-29 13:07:37 +0700

    update

--- dist/reactapp_module/index.js
+++ dist/reactapp_module/index.js
@@ -1,8 +1,48 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.create_reactapp = void 0;
-var inquirer = require("inquirer");
+const inquirer_1 = __importDefault(require("inquirer"));
+const child_process_1 = require("child_process");
+var ncmd = require("node-cmd");
 function create_reactapp() {
-    console.log("anda akan membuat aplikasi reactjs");
+    inquirer_1.default
+        .prompt([
+        {
+            type: "input",
+            name: "react_app_name",
+            message: "masukkan nama aplikasi react anda?",
+            filter: (input) => {
+                return new Promise((resolve, reject) => {
+                    if (input == "") {
+                        reject("invalid");
+                    }
+                    else {
+                        resolve(input);
+                    }
+                });
+            },
+        },
+    ])
+        .then((res) => {
+        let name = res.react_app_name;
+        console.log("anda akan membuat applikasi reactjs dengan nama " + name + "\n");
+        const create_app = child_process_1.spawn("npx", ["create-react-app", name], {
+            cwd: process.cwd(),
+            detached: true,
+            stdio: "inherit",
+        });
+        create_app.on("exit", () => {
+            console.log("exit create process \n");
+            console.log("begin remove dir \n");
+            child_process_1.spawn("rm", ["-rf", name], {
+                cwd: process.cwd(),
+                detached: true,
+                stdio: "inherit",
+            });
+        });
+    });
 }
 exports.create_reactapp = create_reactapp;
--- src/reactapp_module/index.ts
+++ src/reactapp_module/index.ts
@@ -1,5 +1,49 @@
-var inquirer = require("inquirer");
+import inquirer from "inquirer";
+import commander from "commander";
+import { stdout } from "process";
+import { stderr } from "chalk";
+import { exec, spawn } from "child_process";
+
+var ncmd = require("node-cmd");
 
 export function create_reactapp() {
-    console.log("anda akan membuat aplikasi reactjs");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "react_app_name",
+                message: "masukkan nama aplikasi react anda?",
+                filter: (input) => {
+                    return new Promise((resolve, reject) => {
+                        if (input == "") {
+                            reject("invalid");
+                        } else {
+                            resolve(input);
+                        }
+                    });
+                },
+            },
+        ])
+        .then((res) => {
+            let name = res.react_app_name;
+            console.log("anda akan membuat applikasi reactjs dengan nama " + name + "\n");
+
+            const create_app = spawn("npx", ["create-react-app", name], {
+                cwd: process.cwd(),
+                detached: true,
+                stdio: "inherit",
+            });
+
+            create_app.on("exit", () => {
+                console.log("exit create process \n");
+
+                console.log("begin remove dir \n");
+
+                spawn("rm", ["-rf", name], {
+                    cwd: process.cwd(),
+                    detached: true,
+                    stdio: "inherit",
+                });
+            });
+        });
 }
